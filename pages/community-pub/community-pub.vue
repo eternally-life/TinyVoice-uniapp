ad460261eb939d5ddac778d6b5519c19e9bc3964
creeper <1021495425@qq.com>
2022-07-24 16:22:10 +0800

    创建新页面

---
+++ pages/community-pub/community-pub.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="community-publish-wrap">
+		<view class="community-publish">
+			<view class="pub-card">
+				
+				<view class="">
+					
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.community-publish-wrap{
+	
+}
+</style>
