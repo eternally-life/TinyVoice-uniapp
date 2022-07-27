fde38afaa90e609eeea88e9ed37b4bb0d5e72fa8
hcy <1799518240@qq.com>
2022-07-27 22:47:14 +0800

    111111111111

---
+++ subpages_shop/order/order.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="container">
+		商城订单
+	</view>
+</template>
+
+<script>
+	import {
+		payTinymallPageOrder_Get
+	} from '@/api/商城模块/商品信息下单.js'
+	export default {
+		data() {
+			return {
+				orderList: []
+			}
+		},
+		onLoad() {
+			this.getShopOrder()
+		},
+		methods: {
+			async getShopOrder() {
+				const res = await payTinymallPageOrder_Get()
+				console.log(res);
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.container []
+</style>
