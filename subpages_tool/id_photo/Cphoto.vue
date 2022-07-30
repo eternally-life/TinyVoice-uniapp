0b2721ba61a117788740e2bde92aef8c033ba25b
钟泽霖 <1448708759@qq.com>
2022-07-30 12:03:18 +0800

    计算机证件照上传

---
+++ subpages_tool/id_photo/Cphoto.vue
@@ -0,0 +1,27 @@
+<template>
+				<u-upload
+					:fileList="fileList3"
+					@afterRead="afterRead"
+					@delete="deletePic"
+					name="3"
+					multiple
+					:maxCount="1"
+					:previewFullImage="true"
+				></u-upload>
+</template>
+
+<script>
+	export default {
+				data(){
+					return{
+						fileList3: [{
+							url: '',
+						}],
+					}
+				}
+				}
+</script>
+
+<style>
+				
+</style>
