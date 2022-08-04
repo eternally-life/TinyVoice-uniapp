bd3a64b0e4ae99960cc3dea8d152799bcc361322
DefterYu <a73014@outlook.com>
2022-08-04 23:58:11 +0800

    完善所有消息已读

--- subpages/myMessage/myMessage.vue
+++ subpages/myMessage/myMessage.vue
@@ -5,7 +5,15 @@
 		</u-sticky>
 		<view class="container">
 			<u-swipe-action>
-				<u-swipe-action-item :options="options1" v-for="(item, index) in msgList" :key="item.msgId">
+				<u-swipe-action-item
+					@click="swipeClick"
+					:name="item.msgId"
+					:options="options1"
+					v-for="(item, index) in msgList"
+					:key="item.msgId"
+					:disabled="nowCrrent == 1"
+					:autoClose="true"
+				>
 					<view class="swipe-action u-border-top u-border-bottom">
 						<view class="swipe-action__content list_content">
 							<view class="avatar"><u-avatar :src="item.avatar"></u-avatar></view>
@@ -18,11 +26,21 @@
 				</u-swipe-action-item>
 			</u-swipe-action>
 		</view>
+		<view class="empty" v-if="msgList.length == 0"><u-empty mode="list"></u-empty></view>
+		<view class="bottom_box" v-show="nowCrrent == 0 && msgList.length != 0">
+			<view class="btn">
+				<u-button type="primary" icon="file-text" text="全部消息设置为已读" @click="allin()"></u-button>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
-import { systemSysmsgPage_Get } from '@/api/SYSTEM/消息提醒.js';
+import {
+	systemSysmsgPage_Get /* 获取消息列表*/,
+	systemSysmsgUpdateById_Post /* 单个消息设置已读 */,
+	systemSysmsgUpdateAll_Post /* 全部已读 */
+} from '@/api/SYSTEM/消息提醒.js';
 export default {
 	data() {
 		return {
@@ -45,35 +63,64 @@ export default {
 	methods: {
 		click(index) {
 			if (index == this.nowCrrent) return;
+			this.msgList = [];
 			this.nowCrrent = index;
-			// this.getNoticeList();
-			console.log('index', index);
+			this.getNoticeList();
+			// console.log('index', index);
+		},
+		swipeClick(e) {
+			console.log('点击', e);
 		},
 		getNoticeList() {
 			systemSysmsgPage_Get({
 				isRead: this.nowCrrent.toString() /** 可为空  1已读,0未读,为空默认获取未读    string required:false */,
 				pageNum: 1 /** 第几页    string required:false */,
-				pageSize: 100 /**, 页码大小    string required:false */
+				pageSize: 50 /**, 页码大小    string required:false */
 			}).then(res => {
-				console.log(this.nowCrrent.toString(), '消息', res.data.data.records);
-				if (res.data.data.records.length > 0) {
-					this.msgList = res.data.data.records;
-				}
+				// console.log(this.nowCrrent.toString(), '消息', res.data.data.records);
+				// if (res.data.data.records.length > 0) {
+				this.msgList = res.data.data.records;
+				// }
 			});
+		},
+		allin() {
+			new Promise((resolve, reject) => {
+				uni.showModal({
+					title: '温馨提示',
+					content: '确定将所有未读消息设置为已读吗？',
+					success: res => {
+						res.confirm && resolve();
+						res.cancel && reject('用户点击取消');
+					}
+				});
+			})
+				.then(() => {
+					console.log('用户点击确定');
+					systemSysmsgUpdateAll_Post().then(res => {
+						console.log('全选结果', res);
+						if (res.data.code == 200) {
+							this.msgList = [];
+						}
+					});
+				})
+				.catch(err => {
+					console.log('中断原因', err);
+				});
 		}
 	}
 };
 </script>
 
 <style scoped lang="scss">
+@import '@/style/commonstyle/address.scss';
 .tabs {
 	margin: 0 auto;
-	width: 60vw;
+	width: 70vw;
 }
 .container {
 	.swipe-action {
 		&__content {
-			padding: 25rpx;
+			padding: 20rpx;
 		}
 		.list_content {
 			display: flex;
@@ -101,5 +148,9 @@ export default {
 			}
 		}
 	}
+}
+.empty {
+	width: 100%;
+	margin: 30vh auto;
 }
 </style>
