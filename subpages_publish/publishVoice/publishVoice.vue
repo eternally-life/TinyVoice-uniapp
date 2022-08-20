a014bd78cb801e82db38a761c75af678ff3b6bc5
zhangjinbo <1308003218@qq.com>
2022-08-20 12:48:35 +0800

    0820

--- subpages_publish/publishVoice/publishVoice.vue
+++ subpages_publish/publishVoice/publishVoice.vue
@@ -5,32 +5,36 @@
         <u--input placeholder="请输入您的标题内容" border="bottom" v-model="voiceTitle"></u--input>
       </view> -->
       <view class="voice_content">
-        <u--textarea v-model="voiceContent" placeholder="请输入您想发布的文本内容" border="none" height="130" focus></u--textarea>
+        <u--textarea v-model="voiceContent" placeholder="请输入您想发布的文本内容" border="none" height="130" focus
+          style="background:#000" :adjustPosition="false"></u--textarea>
         <view class="content_upload">
           <ty-file-upload @upPara="getImgData" paraGetMethods="active" />
-          <view class="publish_btn">
-            <u-button type="primary" @click="sendSamllVoice" text="发布" :customStyle="customStyle"
-              :loading="publishLoading" loadingText="发布中" round></u-button>
-          </view>
         </view>
       </view>
       <view class="publish_type_wrap">
         <text>发布标签：</text>
-        <view class="tag_item">
-          <u-tag @click="taggleTagIndex(1)" :text="activeTagIndex === 1 ? '微音√' : '微音'" type="warning" plain
-            borderColor="#60C5BA" :color="activeTagIndex === 1 ? '#fff' : '#60C5BA'"
-            :bgColor="activeTagIndex === 1 ? '#60C5BA' : '#fff'"></u-tag>
+        <view class="tag_item_right">
+          <view class="tag_item">
+            <u-tag @click="taggleTagIndex(1)" text="微音" type="warning" plain borderColor="#60C5BA"
+              :color="activeTagIndex === 1 ? '#fff' : '#60C5BA'" :bgColor="activeTagIndex === 1 ? '#60C5BA' : '#fff'">
+            </u-tag>
+          </view>
+          <view class="tag_item">
+            <u-tag @click="taggleTagIndex(3)" text="失物招领" type="warning" plain borderColor="#00E600"
+              :color="activeTagIndex === 3 ? '#fff' : '#00E600'" :bgColor="activeTagIndex === 3 ? '#00E600' : '#fff'">
+            </u-tag>
+          </view>
+          <view class="tag_item">
+            <u-tag @click="taggleTagIndex(4)" text="表白" type="warning" plain borderColor="pink"
+              :color="activeTagIndex === 4 ? '#fff' : 'pink'" :bgColor="activeTagIndex === 4 ? 'pink' : '#fff'">
+            </u-tag>
+          </view>
+          <view class="tag_item">
+            <u-tag @click="taggleTagIndex(6)" text="匿名" type="warning" plain borderColor="#000"
+              :color="activeTagIndex === 6 ? '#fff' : '#000'" :bgColor="activeTagIndex === 6 ? '#000' : '#fff'">
+            </u-tag>
+          </view>
         </view>
-        <!-- <view class="tag_item">
-          <u-tag @click="taggleTagIndex(3)" :text="activeTagIndex === 3 ? '二手市场√' : '二手市场'" type="warning" plain
-            borderColor="#60C5BA" :color="activeTagIndex === 3 ? '#fff' : '#60C5BA'"
-            :bgColor="activeTagIndex === 3 ? '#60C5BA' : '#fff'"></u-tag>
-        </view>
-        <view class="tag_item">
-          <u-tag @click="taggleTagIndex(2)" :text="activeTagIndex === 2 ? '寻物√' : '寻物'" type="success" plain
-            borderColor="#60C5BA" :color="activeTagIndex === 2 ? '#fff' : '#60C5BA'"
-            :bgColor="activeTagIndex === 2 ? '#60C5BA' : '#fff'"></u-tag>
-        </view> -->
       </view>
       <view class="choose_date" v-if="activeTagIndex === 2">
         <u-datetime-picker :show="datetimeShow" v-model="chooseDate" mode="date" :formatter="formatter"
@@ -48,6 +52,10 @@
         </view>
       </block>
     </view>
+    <view class="publish_btn">
+      <u-button type="primary" @click="sendSamllVoice" text="发布" :customStyle="customStyle" :loading="publishLoading"
+        loadingText="发布中" round></u-button>
+    </view>
   </view>
 </template>
 
@@ -63,7 +71,8 @@ export default {
       activeTagIndex: 1,
       datetimeShow: false,
       chooseDate: Number(new Date()),
-      imgArr: []
+      imgArr: [],
+      wxUserInfo: {}
     }
   },
   props: {
@@ -79,7 +88,7 @@ export default {
   },
   mounted() {
     this.windowHeight = getApp().globalData.windowHeight
-    console.log(getApp().globalData.phoneNumber);
+    this.wxUserInfo = getApp().globalData.wxUserInfo ? getApp().globalData.wxUserInfo : {}
   },
   methods: {
     sendSamllVoice() {
@@ -87,11 +96,13 @@ export default {
         uni.$u.toast('内容不可为空哦~')
         return
       }
+      // phonenumber
       this.publishLoading = true
       communityTinybbsSave_Post({
         type: this.activeTagIndex,
         content: this.voiceContent,
-        imgList: this.imgArr
+        imgList: this.imgArr,
+        contact: this.activeTagIndex === 3 ? this.wxUserInfo.phonenumber : null
       })
         .then((res) => {
           if (res.data.code === 200) {
@@ -107,7 +118,6 @@ export default {
           uni.$u.toast(err.message)
         })
         .finally(() => {
-          console.log('@')
           this.publishLoading = false
         })
     },
@@ -167,14 +177,11 @@ export default {
   .voice_content {
     .content_upload {
       padding: 20rpx 0 0 20rpx;
-
-      .publish_btn {
-        position: fixed;
-        bottom: 60rpx;
-        left: 20rpx;
-        right: 20rpx;
-      }
     }
+  }
+
+  .publish_btn {
+    margin-top: 30rpx;
   }
 
   .chooose_date_wrap {
@@ -188,17 +195,27 @@ export default {
 
   .publish_type_wrap {
     display: flex;
-    align-items: center;
+    align-items: flex-start;
     padding: 0 20rpx;
 
     text {
+      width: 160rpx;
       font-size: 28rpx;
       color: #60c5ba;
     }
 
-    .tag_item {
-      margin-right: 20rpx;
+    .tag_item_right {
+      display: flex;
+      flex-wrap: wrap;
+
+      .tag_item {
+        margin-right: 20rpx;
+        &:nth-child(n+4){
+          margin-top: 20rpx;
+        }
+      }
     }
+
   }
 }
 </style>
