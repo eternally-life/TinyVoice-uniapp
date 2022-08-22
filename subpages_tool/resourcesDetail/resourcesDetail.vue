d7a2eff6d75a168c68b99a7dad7e85cc4df3938a
creeper <1021495425@qq.com>
2022-08-23 03:13:06 +0800

    资源完善

--- subpages_tool/resourcesDetail/resourcesDetail.vue
+++ subpages_tool/resourcesDetail/resourcesDetail.vue
@@ -18,7 +18,7 @@
 				<view class="comments_border"></view>
 				<view class="comments_content">评论</view>
 			</view>
-			<!-- <view class="not_reply" v-if="this.comments.length === 0 || this.comments">暂无评论</view> -->
+			<view class="not_reply" v-if="comments.length === 0">暂无评论</view>
 			<view class="item_comment" v-for="(item,index) in comments" :key="index">
 				<!-- {{item.avatar}} -->
 				<view class="comment_left">
@@ -36,6 +36,17 @@
 					</view>
 				</view>
 			</view>
+			<view class="comments_input">
+				<u-input class="input" placeholder="快留下对资源的看法吧" border="botton" clearable shape="circle"
+					placeholderStyle="color:#60C5BA" color="#60C5BA" maxlength="100" @confirm="sendVoiceReply"
+					v-model="replyMessage">
+					<template slot="suffix">
+						<view class="input_right">
+							<u-button :customStyle="customStyle" size="small" @click="sendMessage">发送</u-button>
+						</view>
+					</template>
+				</u-input>
+			</view>
 		</view>
 	</view>
 </template>
@@ -51,28 +62,77 @@
 				eventChannel: null,
 				// describe: '',
 				datas: {},
-				comments: {}
+				comments: {},
+				replyMessage: ''
 			};
 		},
+		//	发送框样式
+		props: {
+			customStyle: {
+				type: Object,
+				default: {
+					background: '#60C5BA',
+					color: '#fff',
+					borderRadius: '40rpx',
+				},
+			},
+		},
 		onLoad(option) {
+			//	获取资源详情
 			const eventChannel = this.getOpenerEventChannel();
 			eventChannel.on('acceptResourcesData', data => {
 				this.datas = data.data
 				// console.log(this.datas);
-				console.log(this.datas.resourceId);
+				console.log(this.datas);
 			});
-			communityTinyserveresourceByResourceId_Get({
-				resourceId: this.datas.resourceId
-			}).then(res => {
-				let datas = res.data.data;
-				this.comments = datas
-				console.log(this.comments);
-			})
+		},
+		// onUnload() {
+		// 	this.eventChannel.off('login');
+		// },
+		mounted() {
+			this.getResourcesMessage()
+		},
+		methods: {
+			getResourcesMessage(){
+				// 获取资源评论
+				communityTinyserveresourceByResourceId_Get({
+					resourceId: this.datas.resourceId
+				}).then(res => {
+					let datas = res.data.data;
+					this.comments = datas
+					console.log(this.comments);
+				})
+			},
+			//	发送留言
+			async sendMessage() {
+				if (this.replyMessage === '') {
+					uni.showToast({
+						title: '消息不能为空',
+						icon: 'none'
+					})
+					return
+				}
 
+				const res = await communityTinyserveresourceSaveReply_Post({
+					resourceId: this.datas.resourceId,
+					content: this.replyMessage
+				})
+				console.log(res);
+				if (res.data.code === 200) {
+					uni.showModal({
+						content: '留言成功',
+						showCancel: false
+					});
+					
+					this.getResourcesMessage(res => {
+						this.comments = [res.data.data , ...this.comments]
+					})
+					this.replyMessage = ''
+					
+				}
+			}
 		},
-		onUnload() {
-			this.eventChannel.off('login');
-		},
+
 	}
 </script>
 
@@ -107,9 +167,13 @@
 
 		.box_comment {
 			padding: 30rpx;
-			box-shadow: 0rpx 0rpx 10rpx #ccc;
+			box-shadow: 0rpx 0rpx 5rpx #ccc;
 			position: relative;
 			background-color: #ffffff;
+			margin: 30rpx auto;
+			.not_reply {
+				margin: 30rpx;
+			}
 
 			.comments_title {
 				display: flex;
@@ -136,8 +200,13 @@
 				align-items: flex-start;
 				min-height: 140rpx;
 				padding: 20rpx;
+				margin: 60rpx auto;
 
-				.comment_left {}
+				.comment_left {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+				}
 
 				.comment_right {
 					display: flex;
@@ -147,6 +216,27 @@
 
 				}
 			}
+
+			.comments_input {
+				padding: 10rpx 30rpx;
+				position: fixed;
+				bottom: 0rpx;
+				left: 0;
+				right: 0rpx;
+				z-index: 99;
+				background: #EFF9F8;
+
+				i {
+					color: #69c8be;
+					font-size: 60rpx;
+				}
+
+				.input_right {
+					display: flex;
+					align-items: center;
+				}
+			}
 		}
+
 	}
 </style>
